0eccb1c3d94da9f913dbea3498c44d3eb76627b6
smilekzs <smilekzs@gmail.com>
2013-12-04 20:45:30 +0800

    simple test for drive

--- user/main.cpp
+++ user/main.cpp
@@ -7,51 +7,47 @@ using namespace std;
 #include "conf.hpp"
 #include "misc.hpp"
 
+#include "drive.hpp"
+
 __task void main_task(){
-    PE::enable();
-    PE7.conf(GPIO_OUT_PP);
-    PE7=1;
 
-    PF::enable();
-    PF13.conf(GPIO_IN_PULL);
-    PF13=1;
+    printf(
+        "\r\n\r\n"
+        "### proto-nc-main %s %s\r\n"
+        "\r\n",
+        __DATE__, __TIME__
+    );
+    
+    drive_init();
+    drive_start();
 
-    Exti<13>::enable();
-    Exti<13>::set_falling();
-    PF13.set_exti();
-    NVIC_EnableIRQ(EXTI15_10_IRQn);
+    os_dly_wait(100);
 
-    printf("\r\nHello World!\r\n");
-	int a, b;
-	char c;
-	while(1){
-		c=fgetc(stdin);
-		switch(c){
-			case 'a':
-				if(scanf("%d", &a)==1){
-					printf("<a a=\"%d\" />\r\n", a);
-				}
-				break;
-			case 'b':
-				if(scanf("%d %d", &a, &b)==2){
-					printf("<b a=\"%d\" b=\"%d\" />\r\n", a, b);
-				}
-				break;
-		}
-	}
-	
+    DriveCmd *cmd;
+
+#define SEND(dt, dx, dy) do { \
+    cmd = drive_cmd_pool.allocate(); \
+    cmd->dThms = dt; \
+    cmd->dAp[0] = dx; \
+    cmd->dAp[1] = dy; \
+    os_mbx_send(drive_cmd_mbx, cmd, FOREVER); \
+} while (false)
+
+    SEND(2, 10, 10);
+    SEND(4, 10, -10);
+    SEND(2, -10, -10);
+    SEND(4, -10, 10);
+
+    printf("### done\r\n");
+
+    while (1) ;
+
 	/*
     wait_ms(1000);
     //TODO: Initialize and launch other tasks
 
     os_tsk_delete_self();
 	*/
-}
-
-IRQ_DECL(EXTI15_10_IRQn, EXTI15_10_IRQHandler, 5, 0);
-void EXTI15_10_IRQHandler(){
-    PE7=!PE7;
-    Exti<13>::clear();
 }
 
 int main(){
